df08aa016a5e25bd6fa26ad0b2c4873d9a705ef3
DavidWAREM <116148810+DavidWAREM@users.noreply.github.com>
2023-02-12 20:42:53 +0100

    Update Category 2

--- Category.py
+++ Category.py
@@ -1,53 +1,149 @@
 
+#Beginning Definition of daily amount of water, this will be later delited.
 V_equ = 500
 
-area=input("Is the supplied area a residual (1), a comercial (2) or a industrial (3) area?"
-              "\n Write the number ")
-print(area)
+def category_2():
+    #Getting the first input for the callculation of the requred water amount for a fire event.
+    #the while True checks, if the input is correct
+    while True:
+        try:
+            area = int(input("Is the supplied area a residual (1), a comercial (2) or a industrial (3) area?"
+                 "\n Write the number: "))
+        except ValueError:
+            print("Wrong Input, that is not one of the given options."
+                  "\nPlease insert 1 for residual, 2 for comercial and 3 for industrial area.")
+            continue
+        else:
+            break
 
-if area == "1":
-    floors_number=input("What is the maximum number of floors in the area?")
-    floors_number=int(floors_number)
-    if floors_number <= 3:
+    if area == 1:
+        #If the area is an Residual, this is the branche.
+        #The while True checks, is the input for the floor number is correct.
+        while True:
+            try:
+                floors_number = int(input("What is the maximum number"
+                              " of floors in the area? "))
+            except ValueError:
+                print("Wrong Input, that is not a number!"
+                      "\nPlease insert a number.")
+                continue
+            else:
+                break
 
-        spread_risk=input("Is the risk of fire spread small, medium or high?"
-                     "\nIf you do not know, ask the responsible fire department.")
+        if floors_number <= 3:
+            #
+            spread_risk = input("Is the risk of fire spread in the area small, medium or high?"
+                                "\nIf you do not know, ask the responsible fire department. ")
+
+            while spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
+                spread_risk = input("This is neather 'small', 'medium', or 'high'."
+                                    "\nPlease check the risk of fire spread and insert on of the three terms. ")
+
+            if spread_risk == "small":
+                print("niedrig, small, Residual")
+                V_fire = 2 * 48
+            if spread_risk == "medium":
+                print("niedrig, medium, Residual")
+                V_fire = 2 * 96
+            if spread_risk == "high":
+                print("niedrig, high, Residual")
+                V_fire = 2 * 96
+            if spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
+                print("Wrong input spread risk!")
+
+        else:
+            spread_risk = input("Is the risk of fire spread in the area small, medium or high?"
+                                "\nIf you do not know, ask the responsible fire department. ")
+
+            while spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
+                spread_risk = input("This is neather 'small', 'medium', or 'high'."
+                                    "\nPlease check the risk of fire spread and insert on of the three terms. ")
+
+            if spread_risk == "small":
+                print("hoch, small, Residual")
+                V_fire = 2 * 96
+            if spread_risk == "medium":
+                print("hoch, medium, Residual")
+                V_fire = 2 * 96
+            if spread_risk == "high":
+                print("hoch, high, Residual")
+                V_fire = 2 * 192
+
+    if area == 2:
+        while True:
+            try:
+                floors_number = int(input("What is the maximum number"
+                              " of floors in the area? "))
+            except ValueError:
+                print("Wrong Input, that is not a number!"
+                      "\nPlease insert a number.")
+                continue
+            else:
+                break
+
+        if floors_number <= 1:
+            spread_risk = input("Is the risk of fire spread in the area small, medium or high?"
+                                "\nIf you do not know, ask the responsible fire department. ")
+
+            while spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
+                spread_risk = input("This is neather 'small', 'medium', or 'high'."
+                                    "\nPlease check the risk of fire spread and insert on of the three terms. ")
+
+            if spread_risk == "small":
+                print("niedrig, small, Comercial")
+                V_fire = 2 * 96
+            if spread_risk == "medium":
+                print("niedrig, medium, Comercial")
+                V_fire = 2 * 96
+            if spread_risk == "high":
+                print("niedrig, high, Comercial")
+                V_fire = 2 * 192
+            if spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
+                print("Wrong input spread risk!")
+        else:
+            spread_risk = input("Is the risk of fire spread in the area small, medium or high?"
+                                "\nIf you do not know, ask the responsible fire department. ")
+
+            while spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
+                spread_risk = input("This is neather 'small', 'medium', or 'high'."
+                                    "\nPlease check the risk of fire spread and insert on of the three terms. ")
+
+            if spread_risk == "small":
+                print("hoch, small, Comercial")
+                V_fire = 2 * 96
+            if spread_risk == "medium":
+                print("hoch, medium, Comercial")
+                V_fire = 2 * 192
+            if spread_risk == "high":
+                print("hoch, high, Comercial")
+                V_fire = 2 * 192
+            if spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
+                print("Wrong input spread risk!")
+
+    if area == 3:
+
+        spread_risk = input("Is the risk of fire spread in the area small, medium or high?"
+                            "\nIf you do not know, ask the responsible fire department. ")
+
+        while spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
+            spread_risk = input("This is neather 'small', 'medium', or 'high'."
+                                "\nPlease check the risk of fire spread and insert on of the three terms. ")
+
         if spread_risk == "small":
-            print ("niedrig, small, Residual")
-            V_fire = 2*48
+            print("small, Industrial")
+            V_fire = 2 * 96
         if spread_risk == "medium":
-            print("niedrig, medium, Residual")
-            V_fire = 2*96
+            print("medium, Industrial")
+            V_fire = 2 * 192
         if spread_risk == "high":
-            print("niedrig, high, Residual")
-            V_fire = 2*96
+            print("high, Industrial")
+            V_fire = 2 * 192
         if spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
-            print("Wrong input!")
+            print("Wrong input spread risk!")
 
 
-    else:
-        print("hoch Residual")
-        V_fire = 2*96
+    V_st = V_equ + V_fire
 
-if area == "2":
-    floors_number = input("What is the maximum number of floors in the area?")
-    floors_number = int(floors_number)
-    if floors_number <= 1:
-        print("niedrig Comercial")
-        V_fire = 2 * 48
-    else:
-        print("hoch Comercial")
-        V_fire = 2 * 96
+    print(V_st)
 
-
-if area == "3":
-    print("Industrial")
-    V_fire = 2 * 96
-
-if area != "1" and area != "2" and area != "3":
-    print("Input wrong")
-
-
-V_st = V_equ + V_fire
-
-print(V_st)
+category_2()
--- temp.py
+++ temp.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
-logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+spread_risk = input("Is the risk of fire spread in the area small, medium or high?"
+                            "\nIf you do not know, ask the responsible fire department. ")
+
+while spread_risk != "small" and spread_risk != "medium" and spread_risk != "high":
+        spread_risk = input("This is neather 'small', 'medium', or 'high'."
+                            "\nPlease check the risk of fire spread and insert on of the three terms.")
