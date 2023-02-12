ffa1ccfe7cf63167d1c62e5999f9ed583239af99
DavidWAREM <116148810+DavidWAREM@users.noreply.github.com>
2023-02-12 17:18:10 +0100

    new logging

--- fun.py
+++ fun.py
@@ -1,1 +1,10 @@
 #class to log
+import logging
+
+
+#start logging and set up the roules
+
+def logging ():
+    logging.basicConfig(filename="log_tank_callculation",
+                        format="%(asctime)s - %(message)s", filemode="w",
+                        level=logging.INFO)
