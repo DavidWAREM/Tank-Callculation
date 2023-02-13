ff87c45b3f84cecc5a48d8942b7a328c1c837dd4
ellaGLB <125144079+ellaGLB@users.noreply.github.com>
2023-02-13 17:37:50 +0100

    Merge branch 'v1.0' of C:\Users\lusti\Documents\GitHub\Tank-Callculation with conflicts.

---
+++ plausibility.py
@@ -0,0 +1,21 @@
+import pandas as panda
+import numpy as npy
+
+data = panda.read_excel(r'C:\Users\lusti\Documents\GitHub\Tank-Callculation\data\Verbrauch_2014.xlsx')
+
+data_verbrauch_max = data['Verbrauch'].max()
+date_consumption_max = data['Datum'][data['Verbrauch'] == data_verbrauch_max]
+
+#if date_consumption_max = ""
+#data["Datum"] = data[date_consumption_max].dt.month
+
+print(var)
+
+
+
+print(data_verbrauch_max)
+#print(date_consumption_max)
+#print(date_consumption_max.month)
+#print(data['Datum'][data['Verbrauch'] == data_verbrauch_max])
+
+
