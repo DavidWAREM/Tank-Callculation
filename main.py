221ba49268ceb40105baca7c9ff1ee45666f2890
ashwini mudigoudar <116149034+ashwinimudigoudar@users.noreply.github.com>
2023-02-19 13:47:26 +0100

    Added Comments

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Importing Pandas to use Dataframe and Parse Excel Sheets
 import pandas as panda
 
 def run():
+    # Import Raw Data from Excel Sheets using Dataframe, Repeat for all years 
     data_2014 = panda.read_excel(r'D:\uni_stuttgart\python\pycharmProjects\waterTank\data\Verbrauch_2014.xlsx', index_col=0)
     data_2015 = panda.read_excel(r'D:\uni_stuttgart\python\pycharmProjects\waterTank\data\Verbrauch_2015.xlsx', index_col=0)
     data_2016 = panda.read_excel(r'D:\uni_stuttgart\python\pycharmProjects\waterTank\data\Verbrauch_2016.xlsx', index_col=0)
     data_2017 = panda.read_excel(r'D:\uni_stuttgart\python\pycharmProjects\waterTank\data\Verbrauch_2017.xlsx', index_col=0)
     data_2018 = panda.read_excel(r'D:\uni_stuttgart\python\pycharmProjects\waterTank\data\Verbrauch_2018.xlsx', index_col=0)
     data_2019 = panda.read_excel(r'D:\uni_stuttgart\python\pycharmProjects\waterTank\data\Verbrauch_2019.xlsx', index_col=0)
-
-    data = list()
-    data.append(data_2014)
-    data.append(data_2015)
-    data.append(data_2016)
-    data.append(data_2017)
-    data.append(data_2018)
-    data.append(data_2019)
-
+    
+    # The Excel Sheets have two Columns- Datum and Verbrauch
+    # Now we shall find the Maximum Value from Verbrauch Column for each year 
     data_2014_df_verbrauch_max = data_2014['Verbrauch'].max()
     data_2015_df_verbrauch_max = data_2015['Verbrauch'].max()
     data_2016_df_verbrauch_max = data_2016['Verbrauch'].max()
@@ -28,6 +19,7 @@ def run():
     data_2018_df_verbrauch_max = data_2018['Verbrauch'].max()
     data_2019_df_verbrauch_max = data_2019['Verbrauch'].max()
 
+    # Create an Array of list Containing all Maximum Values from above
     my_data = list()
     my_data.append(data_2014_df_verbrauch_max)
     my_data.append(data_2015_df_verbrauch_max)
@@ -35,14 +27,16 @@ def run():
     my_data.append(data_2017_df_verbrauch_max)
     my_data.append(data_2018_df_verbrauch_max)
     my_data.append(data_2019_df_verbrauch_max)
-
-    qdmax= max(my_data)
-
-    criteria_1 = 0.5 * qdmax
+ 
+    # Find Maximum Value from all years for Discharge 
+    Qdmax= max(my_data)
+ 
+    # First Criteria
+    criteria_1 = 0.5 * Qdmax
     print(f"{criteria_1=}")
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
