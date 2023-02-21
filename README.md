74b91ba8ef6bdda234eab08cb95ee560278004be
ellaGLB <125144079+ellaGLB@users.noreply.github.com>
2023-02-21 16:50:52 +0100

    README format and Plausibilty change

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Tank-Callculation
- Python Group Work
+### Python Programming for Water Resources Engineering and Research
+
+## Final Student Project: Automation of DVGW W300-1 compliant tank design
+
+#picture water tank
+
+>   ***Background***: 
+
+>   ***Goals***:
+
+>   ***Requirements***:
+
+As the demand of an area can change with time the needed volume of a water tank supplying this area may also change. Therefore, using past daily consumption data of the last couple of years as well as the hourly inflow and outflow of the water tank on a peak day a new needed volume should be determined.
--- plausibility.py
+++ plausibility.py
@@ -1,21 +1,43 @@
 import pandas as panda
-import numpy as npy
+import numpy as np
+from datetime import datetime as dt
 
 data = panda.read_excel(r'C:\Users\lusti\Documents\GitHub\Tank-Callculation\data\Verbrauch_2014.xlsx')
+#df = panda.DataFrame(data)
+#print(data)
 
 data_verbrauch_max = data['Verbrauch'].max()
-date_consumption_max = data['Datum'][data['Verbrauch'] == data_verbrauch_max]
 
-#if date_consumption_max = ""
+date_consumption_max = data['Datum'][data['Verbrauch'] == data_verbrauch_max]
+print(date_consumption_max.to_string(index=False))
+#date_consumption_max = data[1][data[2] == data_verbrauch_max]
+#date_test = dt.date(date_consumption_max)
+#print(date_test)
+#the_time_3 = dt.strptime(date_consumption_max, '%m')
+#month = date_consumption_max.month
+#print(month)
+dates = np.array(date_consumption_max)
+check1 = panda.to_datetime(dates).month
+#summer = np.timedelta64(5,'M')
+#print(summer)
+
+if check1 > 5 and check1 < 10:
+    print("Yuhu, Summer!!")
+    print(date_consumption_max['Datum','Verbrauch'])
+else:
+    print("Oops, pipe bursted!!")
+    print(date_consumption_max)
+
+
 #data["Datum"] = data[date_consumption_max].dt.month
 
 #print(var)
 
 #test
 
-print(data_verbrauch_max)
-print(date_consumption_max)
+#print(data_verbrauch_max)
+#print(panda.to_datetime(dates).month)
 #print(date_consumption_max.month)
-#print(data['Datum'][data['Verbrauch'] == data_verbrauch_max])
+#print (data['Datum'][data['Verbrauch'] == data_verbrauch_max])
 
 
