347fb0d32ebf2122964e8f97e56a41ce6f529b01
ellaGLB <125144079+ellaGLB@users.noreply.github.com>
2023-02-25 22:10:55 +0100

    test

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ data_2017 = pd.read_excel(r'data\Consumption_2017.xlsx')
 data_2018 = pd.read_excel(r'data\Consumption_2018.xlsx')
 data_2019 = pd.read_excel(r'data\Consumption_2019.xlsx')
 
-check_plausibility(data_2014['Datum'], data_2014['Verbrauch'],data_2014,"2014")
-check_plausibility(data_2015['Datum'], data_2015['Verbrauch'],data_2015,"2015")
-check_plausibility(data_2016['Datum'], data_2016['Verbrauch'],data_2016,"2016")
-check_plausibility(data_2017['Datum'], data_2017['Verbrauch'],data_2017,"2017")
-check_plausibility(data_2018['Datum'], data_2018['Verbrauch'],data_2018,"2018")
-check_plausibility(data_2019['Datum'], data_2019['Verbrauch'],data_2019,"2019")
+
 
 if __name__ == '__main__':
+    check_plausibility(data_2014['Datum'], data_2014['Verbrauch'], data_2014, "2014")
+    check_plausibility(data_2015['Datum'], data_2015['Verbrauch'], data_2015, "2015")
+    check_plausibility(data_2016['Datum'], data_2016['Verbrauch'], data_2016, "2016")
+    check_plausibility(data_2017['Datum'], data_2017['Verbrauch'], data_2017, "2017")
+    check_plausibility(data_2018['Datum'], data_2018['Verbrauch'], data_2018, "2018")
+    check_plausibility(data_2019['Datum'], data_2019['Verbrauch'], data_2019, "2019")
     start_logging()
     my_tank = Tank()
 #    my_tank.tank_data()
