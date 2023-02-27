3b61c4646155f13a97a172e12f77806990f3a143
ellaGLB <125144079+ellaGLB@users.noreply.github.com>
2023-02-27 21:21:54 +0100

    update 21:21

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ data_raw_2017 = pd.read_excel(r'data\\Consumption_2017.xlsx')
 data_raw_2018 = pd.read_excel(r'data\\Consumption_2018.xlsx')
 data_raw_2019 = pd.read_excel(r'data\\Consumption_2019.xlsx')
 
-
 print("This algorithm will calculate, if the given tank is big enough for the given outflow data,"
       "\nregarding the German law for water supply facilities.")
 
